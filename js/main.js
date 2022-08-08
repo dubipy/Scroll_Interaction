c9c02ffde8f1f20d6db66ec1b43d19c49cf4b666
dubipy <qnfrha999@naver.com>
2022-08-09 00:46:05 +0900

    playAnimation

--- js/main.js
+++ js/main.js
@@ -21,7 +21,16 @@
         //html 객체 모으기
         //각 구간의 컨테이너 역할 지정
         container: document.querySelector("#scroll-section-0"), //section에 있는 ID값
+        messageA: document.querySelector('#scroll-section-0 .main-message.a'), //클래스 범용화 시켜주기
+        messageB: document.querySelector('#scroll-section-0 .main-message.b'),
+        messageC: document.querySelector('#scroll-section-0 .main-message.c'),
+        messageD: document.querySelector('#scroll-section-0 .main-message.d')
       },
+      //어느시점에 등장하고 빠질것인지 정한다
+      values: {
+        //텍스트 올라고오 내려오는 투명도 지정
+        messageA_opacity: [0, 1] //시작과 끝 값 지정 0 ~ 1
+      }
     },
     {
       // 1
@@ -62,6 +71,41 @@
         i
       ].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`;
     }
+
+    yOffset = window.pageYOffset; //pageYOffset 대신 yOffset변수를 사용하기로했지만 ㅈㅇ확한 의미를 두기 위함
+    //setLayout 엣도 현재 스크롤 위치에 맞게 currentscene을 셋팅해준다
+    let totalScrollHeight = 0;
+    for(let i=0; i < sceneInfo.length; i++) {
+      totalScrollHeight += sceneInfo[i].scrollHeight; //각 scene에 scrollHeight를 ㄷ햇ㅅ ㄶㅇ주는 중
+      //현재 스크롤 높이와 비교한다 for문 돌면 돌수록 스크롤 높이가 ㅋ짐
+      if(totalScrollHeight >= yOffset) { //ㅇㄴ젠가는 ㅁㅁ추ㅇ야 하므로 현재 스크롤 위치보다 큭나 같을 때
+        currentScene = i;
+        break;
+      }
+    }
+     //body에 현재 currentScene 갱신한다
+    document.body.setAttribute('id',`show-scene-${currentScene}`)
+  }
+
+  function playAnimation() {
+    switch (currentScene) {
+      case 0:
+        // console.log('0 play');
+        break;
+
+      case 1:
+        // console.log('1 play');
+        break;
+
+      case 2:
+        // console.log('2 play');
+        break;
+
+      case 3:
+        // console.log('3 play');
+        break;
+    }
+
   }
 
   function scrollLoop() {
@@ -77,24 +121,37 @@
     if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
       //증가할 땐 currentScene이 더해줘야한다
       currentScene++;
+      document.body.setAttribute('id',`show-scene-${currentScene}`)
     }
     if (yOffset < prevScrollHeight) {
       //브라우저에서 스크롤 최상단에서 위로 쭉 땡기면 바운스 효과가 나오는데 사파리 같은 경우에는 마이너스 값으로 취급한다
       //이때 이러한 값 때문에 의도치 않은 효과가 나오므로 사전에 방지한다
       if (currentScene === 0) return; // 이렇게 작성하면 마이너스 값을 취급하는 브라우저에서도 마이너스로 빼지 않고 리턴하며 종료시킨다
       currentScene--;
+      document.body.setAttribute('id',`show-scene-${currentScene}`)
     }
-    console.log(currentScene);
+  //  document.body.setAttribute('id',`show-scene-${currentScene}`) //currentScene의 순서를 낳는다. body의 id가 셋팅된다
+
+  //애니메이션 처리
+  playAnimation();
+
   }
 
-  window.addEventListener("resize", setLayout); //window 창이 사이즈가 변할 때 setLayout 실행
+
   window.addEventListener("scroll", () => {
     // 구체적인 역할을 하는 함수 넣는 곳, 추가적인 함수를 넣기 위해 익명함수로 생성했음
     yOffset = window.pageYOffset; // 스크롤이 일어날 때 pageYOffset값으로 갱신한다
     scrollLoop(); //스크롤 시 내려간다는의미로 생성 // 스크롤 시 scrollLoop가 실행됨
   });
 
-  setLayout();
+  window.addEventListener('load', setLayout); //load 시 setLayout 실행
+  // window.addEventListener('DOMContentLoaded', setLayout);
+  //DOMContentLoaded도 사용 가능하다
+  //load는 웹페이지에 있는 이미지 리소스 모두 싹다 로딩하고 실행되고
+  //DOMContentLoaded는 DOM 답게 DOM 구조만 끝나면 실행된다, 이미지는 상관하지않음 속도측면엣ㄴ DOM을 사용
+  window.addEventListener("resize", setLayout); //window 창이 사이즈가 변할 때 setLayout 실행
+  //setLayou 현재 역할은 sceneInfo 배열에 있는 각 scene의 scrollHeight를 잡아주고 scrollHeight를 값을 실제 scrollsection element의 높이로 셋팅해준다 
+  // setLayout(); //layout 초기화
 })();
 
 // section 요소도 애니메이션에 연관되니 각 scroll-section들이 각 구간의 컨테이너를 의미한다 section도 sceneInfo 배열에 넣는다
